0bef8db50805ea657a82213b2f32a11508597583
Mohamed Shadhaan <shadhanm@gmail.com>
2024-04-03 19:02:27 +0500

    bypass middleware fr

--- middleware.ts
+++ middleware.ts
@@ -1,33 +1,34 @@
 import { NextResponse, type NextRequest } from "next/server";
 import { getMiddlewareClient } from "./lib/supabase";
 
-// export async function middleware(req: NextRequest) {
-//   if (process.env.NODE_ENV === "production") {
-//     let res = NextResponse.next({
-//       request: {
-//         headers: req.headers,
-//       },
-//     });
+export async function middleware(req: NextRequest) {
+  if (process.env.NODE_ENV === "production") {
+    let res = NextResponse.next({
+      request: {
+        headers: req.headers,
+      },
+    });
 
-//     const supabase = getMiddlewareClient(req, res);
-//     const {
-//       data: { session },
-//       error,
-//     } = await supabase.auth.getSession();
+    const supabase = getMiddlewareClient(req, res);
+    const {
+      data: { session },
+      error,
+    } = await supabase.auth.getSession();
 
-//     if (error) {
-//       return NextResponse.json(
-//         { success: false, message: error.message },
-//         { status: error.status },
-//       );
-//     }
+    if (error) {
+      return NextResponse.json(
+        { success: false, message: error.message },
+        { status: error.status },
+      );
+    }
 
-//     if (!session) {
-//       return NextResponse.redirect(new URL("/login", req.url));
-//     }
-//   }
+    if (!session) {
+      return NextResponse.redirect(new URL("/login", req.url));
+    }
+  }
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|login|signup|favicon.ico).*)"],
+  // matcher: ["/((?!_next/static|_next/image|login|signup|favicon.ico).*)"],
+  matcher: ["/api"],
 };
